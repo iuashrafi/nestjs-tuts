07ebc29a2fb3361af3c4abc7a835ecf9c0cbb049
Imtiaz Uddin <iuashrafi@gmail.com>
2024-09-08 00:28:41 +0530

    adding global Middlewares

--- rest-tut-1/src/main.ts
+++ rest-tut-1/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Request, Response, NextFunction } from 'express';
+
+// Global Middlewares
+function globalMiddleWareOne(req: Request, res: Response, next: NextFunction) {
+  console.log('This is the global middleware no 1');
+  next();
+}
+
+function globalMiddleWareTwo(req: Request, res: Response, next: NextFunction) {
+  console.log('This is the global middleware no 2');
+  next();
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // register middlewares
+  app.use(globalMiddleWareOne);
+  app.use(globalMiddleWareTwo);
   await app.listen(3000);
 }
 bootstrap();
